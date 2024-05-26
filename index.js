cea2c9fee6aa09cc315b49498a09a574677d542a
Daniel Garcêz <Daniel_gzribeiro@outlook.com>
2024-05-26 16:19:23 -0300

    V2-01
    Criação do Header, das páginas e da pasta de estilos

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const exphbs = require('express-handlebars')
 const hbs = exphbs.create({
     extname: '.hbs',
     defaultLayout: 'main',
-    partialsDir: __dirname + 'views/partials'
+    partialsDir: __dirname + '/views/partials'
 })
 app.engine('hbs',hbs.engine)
 app.set('view engine', 'hbs')
@@ -15,11 +15,24 @@ app.set('view engine', 'hbs')
 // Servir arquivos estáricos (imagens e estilos)
 app.use(express.static('public'))
 
-// Rotas das páginas .hbs
-app.get('/', function(req, res){
-    res.render('home')
-})
+// Rotas das páginas .hbs:
 
+// home
+app.get('/home', function(req, res){
+    res.render('home', {title:'home'})
+})
+// catalogo
+app.get('/catalogo', function(req, res){
+    res.render('catalogo', {title:'catalogo'})
+})
+// foruns
+app.get('/foruns', function(req, res){
+    res.render('foruns', {title:'foruns'})
+})
+// precos
+app.get('/precos', function(req, res){
+    res.render('precos', {title:'precos'})
+})
 
 
 
