c46f5a9e2d669683fa5534494ec1926403020f8b
mariescbr <mariescbr@hotmail.com>
2024-11-10 22:17:37 -0300

    RetroReads-R11
    - Atualização da page Catálogo: filtros, categorias e faixa de preço
    - Tentativa de ação da seta nos filtros

--- nodeJS/index.js
+++ nodeJS/index.js
@@ -32,3 +32,33 @@ document.querySelectorAll('.olho-senha').forEach(item => {
         }
     });
 });
+
+
+
+
+/* Filtros */
+document.addEventListener("DOMContentLoaded", function() {
+    // Seleciona as seções de Categorias e Faixa de Preço
+    const categoria = document.querySelector('.categoria');
+    const faixaPreco = document.querySelector('.faixa-preco');
+
+    // Seleciona as listas dentro dessas seções
+    const categoriasLista = categoria.querySelector('.categorias-lista');
+    const faixaPrecoLista = faixaPreco.querySelector('.faixa-preco-lista');
+
+    // Seleciona as setas dentro dos elementos h3
+    const setaCategoria = categoria.querySelector('.seta');
+    const setaFaixaPreco = faixaPreco.querySelector('.seta');
+
+    // Função para alternar a visibilidade da lista de categorias e rotacionar a seta
+    categoria.addEventListener('click', () => {
+        categoriasLista.classList.toggle('mostrar'); // Alterna a classe mostrar
+        setaCategoria.style.transform = categoriasLista.classList.contains('mostrar') ? 'rotate(180deg)' : 'rotate(0deg)'; // Rotaciona a seta
+    });
+
+    // Função para alternar a visibilidade da lista de faixa de preço e rotacionar a seta
+    faixaPreco.addEventListener('click', () => {
+        faixaPrecoLista.classList.toggle('mostrar'); // Alterna a classe mostrar
+        setaFaixaPreco.style.transform = faixaPrecoLista.classList.contains('mostrar') ? 'rotate(180deg)' : 'rotate(0deg)'; // Rotaciona a seta
+    });
+});
