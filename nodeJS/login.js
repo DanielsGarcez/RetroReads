bc8c77292d9a8957b5c171b1581af3a4ed23f0c0
Daniel Garcêz <137909210+DanielsGarcez@users.noreply.github.com>
2025-10-30 12:26:59 -0300

    Página de Login #1.8v

--- nodeJS/login.js
+++ nodeJS/login.js
@@ -1,5 +1,4 @@
 /* Bloqueia o botão de 'Recuperar Senha' e 'Entrar' se o conteúdo estiver vazio ou invalido */
-
 function validarCampo(){
     const emailValido = validarUsuario();
     document.getElementById('RecuperarSenha').disabled = !emailValido;
@@ -8,7 +7,7 @@ function validarCampo(){
     document.getElementById("botãoLogin").disabled = !senhaValida;
 }
 
-
+// Função que verifica se o usuário inseriu um email
 function validarUsuario(){    
     const email = document.getElementById("username").value;
     if (!email){
@@ -17,11 +16,22 @@ function validarUsuario(){
     return validarEmail(email);
 }
 
+// Função que valida a senha
 function validarSenha(){
     const senha = document.getElementById("password").value;
     return senha.length > 0;
 }
 
+// Função que valida o email
 function validarEmail(email){
     return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
+
+// Função de login do usuário
+function login(){
+    firebase.auth().signInWithEmailAndPassword("any@email.com", "123456").then(response =>{
+        console.log('Sucesso!', response)
+    }).catch(error =>{
+        console.log('Erro', error)
+    })
 }
