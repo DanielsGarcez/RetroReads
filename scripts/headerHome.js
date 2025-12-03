22ebbc386da052483a5515554ac32f2761006dfe
Daniel Garcêz <Daniel_gzribeiro@outlook.com>
2025-12-03 18:22:29 -0300

    RetroReads #1.1
    - Visual Header Home
    - Criação das páginas Catalogo, Planos e Sobre (pendentes)

---
+++ scripts/headerHome.js
@@ -0,0 +1,12 @@
+// Exporta o Header para todas as páginas
+async function carregarHeader() {
+  try {
+    const resposta = await fetch("../pages/pre-sets/HeaderHome.html");
+    const conteudo = await resposta.text();
+    document.body.insertAdjacentHTML("afterbegin", conteudo);
+  } catch (erro) {
+    console.error("Erro ao carregar o header:", erro);
+  }
+}
+
+carregarHeader();
