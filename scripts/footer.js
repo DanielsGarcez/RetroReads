e86de14aa3d7e57d8d3615d82635fd6368fb4019
Daniel Garcêz <Daniel_gzribeiro@outlook.com>
2025-12-04 20:41:17 -0300

    RetroReads #3.0
    - Conclusão do Fetch Footer

---
+++ scripts/footer.js
@@ -0,0 +1,12 @@
+// Exporta o Footer para todas as páginas
+async function carregarFooter() {
+  try {
+    const resposta = await fetch("../pages/pre-sets/Footer.html");
+    const conteudo = await resposta.text();
+    document.body.insertAdjacentHTML("afterend", conteudo);
+  } catch (erro) {
+    console.error("Erro ao carregar o header:", erro);
+  }
+}
+
+carregarFooter();
