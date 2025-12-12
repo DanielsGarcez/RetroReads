2653052d24d7120906b7ef4bd955cd6b559b1313
Daniel Garcêz <137909210+DanielsGarcez@users.noreply.github.com>
2025-12-12 12:55:14 -0300

    RetroReads #8.0
    - Adição do Firebase.js
    - Página Detalhes do Livro (Sem script)
    - Página Catálogo (Sem script)
    - Página Assinatura (Sem script)

---
+++ scripts/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAzQrm0ppeImY2JOueHKSiQOYl7P_NMgcY",
+    authDomain: "retroreads-556a0.firebaseapp.com",
+    projectId: "retroreads-556a0",
+    storageBucket: "retroreads-556a0.firebasestorage.app",
+    messagingSenderId: "137045514283",
+    appId: "1:137045514283:web:12062091dea60a92af726e"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { app, db, auth };
