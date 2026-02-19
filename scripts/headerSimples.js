0eefd29224103ef3dba392bdef134883a97423df
Daniel Garcêz <137909210+DanielsGarcez@users.noreply.github.com>
2026-02-19 13:08:06 -0300

    RetroReads #25.0
    - Teste de header simples para verificação de autenticação (#2)
    + commit versão anterior: RetroReads #24.7

--- scripts/headerSimples.js
+++ scripts/headerSimples.js
@@ -13,19 +13,22 @@ async function carregarHeader() {
 }
 carregarHeader();
 
+const linkPerfilUser = document.getElementById("header-simples-user");
+
 document.addEventListener("DOMContentLoaded", () => {
 
-  onAuthStateChanged(auth, (user) => {
-    const iconPerfil = document.querySelector(".user-perfil");
+  linkPerfilUser.addEventListener("click", (e) => {
+    onAuthStateChanged(auth, (user) => {
+      const iconPerfil = document.querySelector(".user-perfil");
 
-    if (user) {
-      console.log(`Logado como: ${user.email}.`);
-      iconPerfil.href = `/RetroReads/pages/perfilUser.html?id=${user.uid}`;
+      if (user) {
+        console.log(`HeaderSimples: Logado como: ${user.email}.`);
+        iconPerfil.href = `/RetroReads/pages/userPerfil.html?id=${user.uid}`;
 
-    } else {
-      console.log("Usuário não logado.");
-      iconPerfil.href = "/RetroReads/pages/login.html";
-    }
+      } else {
+        console.log("Usuário não logado.");
+        iconPerfil.href = "/RetroReads/pages/login.html";
+      }
+    });
   });
-
 });
